86bd98c1f054b132d970674b58fe4a6808ecac1b
Jack Halten Fahnestock <buttface@jackf.me>
2016-08-22 23:37:25 -0400

    Add script to change color scheme daily

--- _assets/js/custom.js
+++ _assets/js/custom.js
@@ -1,6 +1,7 @@
 
 var description = document.getElementsByClassName("site-description");
 var container   = document.getElementsByClassName("site-description-container");
+var html        = document.getElementsByTagName("html")[0];
 
 function hideDescription() {
   description[0].classList.add("hiding");
@@ -9,4 +10,38 @@ function hideDescription() {
 function toggleDescription() {
   description[0].classList.toggle("hiding");
   container[0].classList.toggle("bring-to-front");
+}
+
+
+function changeColorScheme() {
+  var d = new Date().getDay();
+
+  // Monday
+  if (d == 1) {
+    html.classList.add("red");
+
+  // Tuesday
+  } else if (d == 2) {
+    html.classList.add("orange");
+
+  // Wednesday
+  } else if (d == 3) {
+    html.classList.add("gold");
+
+  // Thursday
+  } else if (d == 4) {
+    html.classList.add("green");
+
+  // Friday
+  } else if (d == 5) {
+    html.classList.add("blue");
+
+  // Saturday
+  } else if (d == 6) {
+    html.classList.add("magenta");
+
+  // Sunday
+  } else if (d == 7) {
+    html.classList.add("black");
+  }
 }
