ce02087c0980e322fa9fe21e9775531986b48be2
Jack Halten Fahnestock <buttface@jackf.me>
2016-08-26 00:41:25 -0400

    Remove dynamic color scheme functionality

--- _assets/js/custom.js
+++ _assets/js/custom.js
@@ -1,7 +1,6 @@
 
 var description = document.getElementsByClassName("site-description");
 var container   = document.getElementsByClassName("site-description-container");
-var html        = document.getElementsByTagName("html")[0];
 
 function hideDescription() {
   description[0].classList.add("hiding");
@@ -10,38 +9,4 @@ function hideDescription() {
 function toggleDescription() {
   description[0].classList.toggle("hiding");
   container[0].classList.toggle("bring-to-front");
-}
-
-
-function changeColorScheme() {
-  var d = new Date().getDay();
-
-  // Monday
-  if (d == 1) {
-    html.classList.add("red");
-
-  // Tuesday
-  } else if (d == 2) {
-    html.classList.add("orange");
-
-  // Wednesday
-  } else if (d == 3) {
-    html.classList.add("banana");
-
-  // Thursday
-  } else if (d == 4) {
-    html.classList.add("olive");
-
-  // Friday
-  } else if (d == 5) {
-    html.classList.add("blue");
-
-  // Saturday
-  } else if (d == 6) {
-    html.classList.add("magenta");
-
-  // Sunday
-  } else if (d == 7) {
-    html.classList.add("black");
-  }
 }
