686037fd829ef7cd23036894a67fe52329c0c833
Jack Halten Fahnestock <buttface@jackf.me>
2016-08-29 23:30:10 -0400

    Rebuild info show/hide functionality

--- _assets/js/custom.js
+++ _assets/js/custom.js
@@ -1,12 +1,30 @@
 
-var description = document.getElementsByClassName("site-description");
-var container   = document.getElementsByClassName("site-description-container");
+var info   = document.getElementsByClassName("info");
+var icon   = document.getElementsByClassName("info-icon");
+var hiding = "hiding-info";
 
-function hideDescription() {
-  description[0].classList.add("hiding");
+function hideInfo() {
+  // position absolute to overlay content
+  info[0].classList.add("absolute");
+
+  // hide info
+  info[0].classList.add(hiding);
+
+  // add show icon
+  icon[0].innerHTML = "+";
 }
 
-function toggleDescription() {
-  description[0].classList.toggle("hiding");
-  container[0].classList.toggle("bring-to-front");
+function toggleInfo() {
+  // apply animation
+  info[0].classList.add("trans");
+
+  // toggle showing/hiding info
+  info[0].classList.toggle(hiding);
+
+  // if hiding, use show icon(+); else, use hide icon(-)
+  if (info[0].classList.contains(hiding)) {
+    icon[0].innerHTML = "+";
+  } else {
+    icon[0].innerHTML = "-";
+  }
 }
