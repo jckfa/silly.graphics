ed18bae4d40aef28e0c45fa4c7d5bdc3e85e7a6f
Jack Halten Fahnestock <buttface@jackf.me>
2016-09-11 00:37:11 -0400

    Set up +/- icons for open/closed lists in index

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,6 +1,14 @@
 
 var ListContent = document.getElementsByClassName("list-content"),
+    listIcon    = document.getElementsByClassName("list-icon"),
     listTitle   = document.getElementsByClassName("list-title");
+
+function initMainDropdown() {
+  // add open icon
+  for (i = 0; i < listIcon.length; i++) {
+    listIcon[i].innerHTML = "+";
+  }
+}
 
 // hide all list items
 function closeLists() {
@@ -10,15 +18,21 @@ function closeLists() {
 }
 
 // show/hide list items
-function toggleList(index) {
+function toggleMainList(index) {
   // if selected item is hiding
   if (ListContent[index].classList.contains("over-800")) {
     // hide all items
     closeLists();
     // and only show selected item
     ListContent[index].classList.remove("over-800");
+
+    // add close icon
+    listIcon[index].innerHTML = "-";
   } else {
     // hide selected item
     ListContent[index].classList.add("over-800");
+
+    // add open icon
+    listIcon[index].innerHTML = "+";
   }
 }
