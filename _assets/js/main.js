a67652a018bdbfdd321a9be49a919889061bb3b9
Jack Halten Fahnestock <buttface@jackf.me>
2016-09-14 19:01:42 -0400

    Add show/hide animation to main lists

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,31 +1,56 @@
 
-var mainListContent = document.getElementsByClassName("main-list-content"),
-    mainListIcon    = document.getElementsByClassName("main-list-icon");
-    over800class    = "over-800"
+var yo             = document.getElementsByClassName("yo"),
+    mainListIcon   = document.getElementsByClassName("main-list-icon"),
+    childContainer = document.getElementsByClassName("child-container"),
+    over800class   = "height0";
+
 
 function closeMainLists() {
-  for (i = 0; i < mainListContent.length; i++) {
+  for (i = 0; i < yo.length; i++) {
     // close all index lists
-    mainListContent[i].classList.add(over800class);
+    yo[i].classList.add(over800class);
     // add open icon to all index lists
     mainListIcon[i].innerHTML = "+";
+    // unindent lists and set opacity to 0
+    childContainer[i].classList.add("unindented");
+    // hide with inline height
+    yo[i].style.height = "0";
   }
 }
 
 // open/close index list
-function toggleMainList(index) {
+function toggleMainList(index, size) {
+  // bleh = window.getComputedStyle(yo[0], null).getPropertyValue("font-size");
+  // label[0].innerHTML = bleh;
+
   // if selected list is closed
-  if (mainListContent[index].classList.contains(over800class)) {
+  if (yo[index].classList.contains(over800class)) {
     // close all index lists
     closeMainLists();
     // only open selected list
-    mainListContent[index].classList.remove(over800class);
+    yo[index].classList.remove(over800class);
     // only add close icon to selected list
     mainListIcon[index].innerHTML = "-";
+    // indent selected list and set opacity to 1
+    childContainer[index].classList.remove("unindented");
+
+    // top, bottom padding: 1.5rem + 1.5rem = 3rem
+    // text: 1em
+    // top border for each item and container: 0.1rem
+    // yo[index].style.height = "calc(" + size + " * (3.1rem + 1em))";
+    // yo[index].style.height = "calc(" + size + " * (" + bleh + " + 0.1rem))";
+    // yo[index].style.height = "calc(" + size + " * (2.8rem + (var(--target-area) * 2) + 0.1rem))";
+    // 1em = 22px. Why? Dunno. But I need 28px (2.8rem) and 22 + 6 gets me that
+    yo[index].style.height = "calc(" + size + " * (1em + 6px + (var(--target-area) * 2) + 0.1rem))";
   } else {
     // only close selected list
-    mainListContent[index].classList.add(over800class);
+    yo[index].classList.add(over800class);
     // only add open icon to selected list
     mainListIcon[index].innerHTML = "+";
+    // unindent selected list and set opacity to 0
+    childContainer[index].classList.add("unindented");
+
+    // hide with inline height
+    yo[index].style.height = "0";
   }
 }
