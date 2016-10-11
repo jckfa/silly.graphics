1c6061a29e4676df3eb336d0ae27dc10d2a5ac26
Jack Halten Fahnestock <buttface@jackf.me>
2016-10-11 02:35:08 -0400

    Rename

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,14 +1,14 @@
 
-var mainListContContainer = document.getElementsByClassName("main-list-content-container"),
-    mainListCont = document.getElementsByClassName("main-list-content"),
-    mainListIcon = document.getElementsByClassName("main-list-icon"),
-    over800class = "height0";
+var main_container = document.getElementsByClassName("main-container"),
+    mainListCont   = document.getElementsByClassName("main-list-content"),
+    mainListIcon   = document.getElementsByClassName("main-list-icon"),
+    over800class   = "height0";
 
 
 function closeMainLists() {
-  for (i = 0; i < mainListContContainer.length; i++) {
+  for (i = 0; i < main_container.length; i++) {
     // close all index lists
-    mainListContContainer[i].classList.add(over800class);
+    main_container[i].classList.add(over800class);
     // add open icon to all index lists
     mainListIcon[i].innerHTML = "+";
   }
@@ -18,18 +18,19 @@ function closeMainLists() {
 function toggleMainList(index, size) {
 
   // if selected list is closed
-  if (mainListContContainer[index].classList.contains(over800class)) {
+  if (main_container[index].classList.contains(over800class)) {
     // close all index lists
     closeMainLists();
     // only open selected list
-    mainListContContainer[index].classList.remove(over800class);
+    main_container[index].classList.remove(over800class);
     // only add close icon to selected list
     mainListIcon[index].innerHTML = "-";
 
-    mainListContContainer[index].style.height = "calc(" + size + " * (1em + 6px + (var(--target-area) * 2) + 0.1rem))";
+    main_container[index].style.height =
+    "calc(" + size + " * (1em + 6px + (var(--target-area) * 2) + 0.1rem))";
   } else {
     // only close selected list
-    mainListContContainer[index].classList.add(over800class);
+    main_container[index].classList.add(over800class);
     // only add open icon to selected list
     mainListIcon[index].innerHTML = "+";
   }
