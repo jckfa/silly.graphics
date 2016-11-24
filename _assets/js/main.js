2b1b0974b061403479f526d88e4b2b37a5ac0b90
Jack Halten Fahnestock <buttface@jackf.me>
2016-11-23 23:10:34 -0500

    Clean up

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -2,7 +2,6 @@
 var list        = document.getElementsByClassName("list"),
     hiding_list = "hiding_list";
 
-
 function close_lists() {
   for (i = 0; i < list.length; i++) {
     list[i].classList.add(hiding_list);
@@ -10,7 +9,6 @@ function close_lists() {
 }
 
 function toggle_list(index) {
-
   if (list[index].classList.contains(hiding_list)) {
     close_lists();
     list[index].classList.remove(hiding_list);
