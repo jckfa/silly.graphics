26393258344ebd81cf39e8f5fae42ead40ba74de
Jack Halten Fahnestock <buttface@jackf.me>
2016-12-02 18:15:17 -0500

    Set up smart nav (fix on scroll, get current section)

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,5 +1,9 @@
 
 var list        = document.getElementsByClassName("list"),
+    container   = document.getElementsByClassName('list-container')[0],
+    nav         = document.getElementsByClassName('site__nav')[0],
+    nav_item    = document.getElementsByClassName('nav-item'),
+    list_title  = document.getElementsByClassName('list-title'),
     hiding_list = "hiding_list";
     selected    = "selected";
 
@@ -18,5 +22,37 @@ function toggle_list(index) {
   } else {
     list[index].classList.add(hiding_list);
     list[index].classList.remove(selected);
+  }
+}
+
+
+var lime = document.getElementsByClassName('lime')[0];
+
+window.addEventListener("resize", smart_nav);
+window.addEventListener("scroll", smart_nav);
+
+function smart_nav() {
+
+  var scroll_pos = document.body.scrollTop,
+      nav_pos    = container.offsetTop;
+  lime.innerHTML = nav_pos + " " + scroll_pos;
+
+  if (scroll_pos >= nav_pos) {
+    nav.classList.add("fix");
+  } else {
+    nav.classList.remove("fix");
+  }
+
+
+  for (i = 0; i < list.length; i++ ) {
+    var list_pos = list_title[i].offsetTop;
+    // list_title[i].innerHTML = list_title[i].offsetTop + nav_pos;
+
+    // -1 compensates for frag rounding bug
+    if (scroll_pos >= nav_pos + list_pos - 1) {
+      nav_item[i].classList.add("current");
+    } else {
+      nav_item[i].classList.remove("current");
+    }
   }
 }
