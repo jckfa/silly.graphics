af9aab0cbf7ae0967c6a4b75040ba337669f26e8
Jack Halten Fahnestock <buttface@jackf.me>
2016-12-02 15:58:09 -0500

    Rework list content

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,10 +1,12 @@
 
 var list        = document.getElementsByClassName("list"),
     hiding_list = "hiding_list";
+    selected    = "selected";
 
 function close_lists() {
   for (i = 0; i < list.length; i++) {
     list[i].classList.add(hiding_list);
+    list[i].classList.remove(selected);
   }
 }
 
@@ -12,7 +14,9 @@ function toggle_list(index) {
   if (list[index].classList.contains(hiding_list)) {
     close_lists();
     list[index].classList.remove(hiding_list);
+    list[index].classList.add(selected);
   } else {
     list[index].classList.add(hiding_list);
+    list[index].classList.remove(selected);
   }
 }
