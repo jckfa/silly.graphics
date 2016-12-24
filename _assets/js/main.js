a55885996fb0e4d1ce039ec741376cf32cf410d7
Jack Halten Fahnestock <buttface@jackf.me>
2016-12-23 23:25:21 -0500

    clean up, simplify, rename stuff, clearify js

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,27 +1,31 @@
 
-var list        = document.getElementsByClassName("list"),
-    container   = document.getElementsByClassName('list-container')[0],
-    list_nav    = document.getElementsByClassName('list__nav')[0],
-    nav_item    = document.getElementsByClassName('nav-item'),
-    list_title  = document.getElementsByClassName('content-shell'),
-    top_frag    = document.getElementsByClassName('top-frag')[0],
-    hiding_list = "hiding_list";
+var main        = document.getElementsByTagName("main")[0],
+    list        = document.getElementsByClassName("list"),
+    list_nav    = document.getElementsByClassName("list__nav")[0],
+    nav_item    = document.getElementsByClassName("nav-item"),
+    top_frag    = document.getElementsByClassName("top-frag")[0],
+    closed_list = "closed_list",
     selected    = "selected";
 
+// close all lists, remove selected class
 function close_lists() {
   for (i = 0; i < list.length; i++) {
-    list[i].classList.add(hiding_list);
+    list[i].classList.add(closed_list);
     list[i].classList.remove(selected);
   }
 }
 
+// toggle selected list
 function toggle_list(index) {
-  if (list[index].classList.contains(hiding_list)) {
+  // if list is closed
+  if (list[index].classList.contains(closed_list)) {
     close_lists();
-    list[index].classList.remove(hiding_list);
+    // open list, add selected class
+    list[index].classList.remove(closed_list);
     list[index].classList.add(selected);
   } else {
-    list[index].classList.add(hiding_list);
+    // close list, remove selected class
+    list[index].classList.add(closed_list);
     list[index].classList.remove(selected);
   }
 }
@@ -31,22 +35,23 @@ window.addEventListener("resize", smart_nav);
 window.addEventListener("scroll", smart_nav);
 
 function smart_nav() {
-
   var scroll_pos = document.body.scrollTop,
-      nav_pos    = container.offsetTop;
+      nav_pos    = main.offsetTop;
 
-  // fix list-nav
+  // fix list-nav if scrolled to/past it
   if (scroll_pos >= nav_pos) {
+    // fix nav, show top frag
     list_nav.classList.add("fixed-nav");
     top_frag.classList.add("showing-frag");
   } else {
+    // unfix nav, hide top frag
     list_nav.classList.remove("fixed-nav");
     top_frag.classList.remove("showing-frag");
   }
 
-
+  // highlight nav item when scrolled to/past respective list
   for (i = 0; i < list.length; i++ ) {
-    var list_pos = list_title[i].offsetTop;
+    var list_pos = list[i].offsetTop;
 
     // -1 compensates for frag rounding bug
     if (scroll_pos >= nav_pos + list_pos - 1) {
