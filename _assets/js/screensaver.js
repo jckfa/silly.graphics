b95f917cc0d0e3a78e21d191706b494c69694494
Jack Halten Fahnestock <buttface@jackf.me>
2016-12-27 20:19:13 -0500

    add screensaver

---
+++ _assets/js/screensaver.js
@@ -0,0 +1,56 @@
+var screensaver = document.getElementsByClassName("screensaver")[0],
+    t,
+    i,
+    wait_time   = 30000; // 30 sec
+
+// prefix single-digit numbers with zero
+function zero_pad(n) {
+  return (n < 10) ? "0" + n : n;
+}
+
+// create clock
+function clock() {
+  var d       = new Date(),
+      h24     = d.getHours(),
+      h12     = (h24 + 24) % 12 || 12,
+      h       = zero_pad(h12),
+      m       = zero_pad(d.getMinutes()),
+      s       = zero_pad(d.getSeconds()),
+      ampm    = (h24 < 12) ? "am" : "pm",
+      format  = h + ":" + m + ":" + s + " " + ampm,
+      content = document.getElementsByClassName("screensaver-content")[0];
+
+  content.innerHTML = format;
+}
+
+// print screensaver content
+function start_timer() {
+  // wait before showing screensaver
+  t = setTimeout( function() {
+    // show screensaver
+    screensaver.classList.remove("hidden");
+    // run clock immediately after wait_time
+    clock();
+    // refresh clock every second
+    i = setInterval(function() {
+      clock();
+    }, 1000);
+  }, wait_time);
+}
+
+function reset_timer() {
+  // hide screensaver content
+  screensaver.classList.add("hidden");
+
+  // clear timers
+  clearTimeout(t);
+  clearInterval(i);
+
+  // restart timer
+  start_timer();
+}
+
+window.addEventListener("load",      start_timer);
+window.addEventListener("mousemove", reset_timer);
+window.addEventListener("scroll",    reset_timer);
+window.addEventListener("resize",    reset_timer);
