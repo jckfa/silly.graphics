6156163100ea4542f38b6aaa6542983554d781d1
Jack Halten Fahnestock <buttface@jackf.me>
2017-01-03 01:01:55 -0500

    reorganize js; only run smart_nav on desktop

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -4,53 +4,44 @@ var list        = document.getElementsByClassName("list"),
     selected    = "selected",
     list_anim   = 250;
 
-// close all lists
-function close_lists() {
-  for (i = 0; i < list.length; i++) {
-    items_shell[i].style.height = "0";
-    list[i].classList.remove(selected);
-  }
-}
+// adjust height on font-size/padding change
+window.addEventListener("resize", set_list_height);
+window.addEventListener("resize", smart_nav);
+window.addEventListener("scroll", smart_nav);
 
-// return true if any lists are open
-function is_any_list_open() {
-  for (i = 0; i < list.length; i++) {
-    if (list[i].classList.contains(selected)) {
-      return true;
+function smart_nav() {
+  // if desktop
+  if (window.innerWidth >= 800) {
+    var main       = document.getElementsByTagName("main")[0],
+        list_nav   = document.getElementsByClassName("list__nav")[0],
+        top_frag   = document.getElementsByClassName("top-frag")[0],
+        scroll_pos = document.body.scrollTop,
+        nav_pos    = main.offsetTop;
+
+    // fix list-nav if scrolled to/past it
+    if (scroll_pos >= nav_pos) {
+      // fix nav, show top frag
+      list_nav.classList.add("fixed-nav");
+      top_frag.classList.add("showing-frag");
+    } else {
+      // unfix nav, hide top frag
+      list_nav.classList.remove("fixed-nav");
+      top_frag.classList.remove("showing-frag");
+    }
+
+    // highlight nav item when scrolled to/past respective list
+    for (i = 0; i < list.length; i++ ) {
+      var nav_item = document.getElementsByClassName("nav-item"),
+          list_pos = list[i].offsetTop;
+
+      // -1 compensates for frag rounding bug
+      scroll_pos >= nav_pos + list_pos - 1 ?
+        nav_item[i].classList.add("current")
+        :
+        nav_item[i].classList.remove("current")
+      ;
     }
   }
-}
-
-// open list
-function set_list_height() {
-  var list_items = document.getElementsByClassName("list-items");
-
-  for (i = 0; i < list.length; i++) {
-    if (list[i].classList.contains(selected)) {
-      items_shell[i].style.height = list_items[i].offsetHeight + "px";
-    }
-  }
-}
-
-// scroll to selected list
-function scroll_to_list(i) {
-  // wait for list to open
-  setTimeout(function() {
-    // get list top position
-    var elem_pos = items_shell[i].offsetTop;
-    // scroll to top of list/to list title
-    window.scrollTo(0, elem_pos);
-  }, list_anim);
-}
-
-// open selected list
-function open_list(i) {
-  // add class for animating open/close icon and list color
-  list[i].classList.add(selected);
-  // open list (set height so opening can be animated)
-  set_list_height();
-  // scroll to selected list
-  scroll_to_list(i);
 }
 
 // toggle selected list
@@ -77,39 +68,51 @@ function toggle_list(i) {
   }
 }
 
-function smart_nav() {
-  var main       = document.getElementsByTagName("main")[0],
-      list_nav   = document.getElementsByClassName("list__nav")[0],
-      top_frag   = document.getElementsByClassName("top-frag")[0],
-      scroll_pos = document.body.scrollTop,
-      nav_pos    = main.offsetTop;
-
-  // fix list-nav if scrolled to/past it
-  if (scroll_pos >= nav_pos) {
-    // fix nav, show top frag
-    list_nav.classList.add("fixed-nav");
-    top_frag.classList.add("showing-frag");
-  } else {
-    // unfix nav, hide top frag
-    list_nav.classList.remove("fixed-nav");
-    top_frag.classList.remove("showing-frag");
-  }
-
-  // highlight nav item when scrolled to/past respective list
-  for (i = 0; i < list.length; i++ ) {
-    var nav_item = document.getElementsByClassName("nav-item"),
-        list_pos = list[i].offsetTop;
-
-    // -1 compensates for frag rounding bug
-    scroll_pos >= nav_pos + list_pos - 1 ?
-      nav_item[i].classList.add("current")
-      :
-      nav_item[i].classList.remove("current")
-    ;
+// return true if any lists are open
+function is_any_list_open() {
+  for (i = 0; i < list.length; i++) {
+    if (list[i].classList.contains(selected)) {
+      return true;
+    }
   }
 }
 
-// adjust height on font-size/padding change
-window.addEventListener("resize", set_list_height);
-window.addEventListener("resize", smart_nav);
-window.addEventListener("scroll", smart_nav);
+// close all lists
+function close_lists() {
+  for (i = 0; i < list.length; i++) {
+    items_shell[i].style.height = "0";
+    list[i].classList.remove(selected);
+  }
+}
+
+// open selected list
+function open_list(i) {
+  // add class for animating open/close icon and list color
+  list[i].classList.add(selected);
+  // open list (set height so opening can be animated)
+  set_list_height();
+  // scroll to selected list
+  scroll_to_list(i);
+}
+
+// open list
+function set_list_height() {
+  var list_items = document.getElementsByClassName("list-items");
+
+  for (i = 0; i < list.length; i++) {
+    if (list[i].classList.contains(selected)) {
+      items_shell[i].style.height = list_items[i].offsetHeight + "px";
+    }
+  }
+}
+
+// scroll to selected list
+function scroll_to_list(i) {
+  // wait for list to open
+  setTimeout(function() {
+    // get list top position
+    var elem_pos = items_shell[i].offsetTop;
+    // scroll to top of list/to list title
+    window.scrollTo(0, elem_pos);
+  }, list_anim);
+}
